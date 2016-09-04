f7d90ffff52965986df177f9e70e46ba68d97d66
hogvarce <hogvarce@gmail.com>
2016-09-04 20:14:53 +0300

    first commit

--- components/App.js
+++ components/App.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react';
+import TextDisplay from './TextDisplay';
 
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <div>This App module</div>
+                <div>Second line</div>
+                <TextDisplay />
+            </div>
+        )
+    }
+}
